5db2798d23669994ffc752d40d236b2bf3fbfcea
Sudhanshu Srivastava <48932266+sudsho@users.noreply.github.com>
2020-12-05 23:12:34 +0530

    added welch t-test

--- src/freq.py
+++ src/freq.py
@@ -1,8 +1,8 @@
 """
 Frequentist hypothesis tests for A/B testing.
 
-Right now: two-proportion z-test.
-TODO: t-test for continuous, chi-square.
+z-test for proportions, Welch's t-test for continuous metrics (revenue, time on page,
+etc.). chi-square added separately.
 """
 from collections import namedtuple
 import math
@@ -55,4 +55,51 @@ def two_proportion_ztest(success_a, n_a, success_b, n_b, alternative="two-sided"
         ci_low=None,   # filled in later
         ci_high=None,
         method="two-proportion z",
+    )
+
+
+def welch_ttest(values_a, values_b, alternative="two-sided"):
+    """Welch's t-test (unequal variances) for continuous metrics.
+
+    Useful for revenue per user, session duration, etc., where the per-arm
+    variance is rarely equal.  Returns a TestResult.
+    """
+    a = np.asarray(values_a, dtype=float)
+    b = np.asarray(values_b, dtype=float)
+    if a.size < 2 or b.size < 2:
+        raise ValueError("each arm needs at least 2 observations")
+
+    mean_a = a.mean()
+    mean_b = b.mean()
+    var_a = a.var(ddof=1)
+    var_b = b.var(ddof=1)
+
+    se = math.sqrt(var_a / a.size + var_b / b.size)
+    if se == 0.0:
+        t = 0.0
+        df = a.size + b.size - 2
+    else:
+        t = (mean_b - mean_a) / se
+        # Welch-Satterthwaite df
+        num = (var_a / a.size + var_b / b.size) ** 2
+        den = (var_a ** 2) / ((a.size ** 2) * (a.size - 1)) + \
+              (var_b ** 2) / ((b.size ** 2) * (b.size - 1))
+        df = num / den if den > 0 else (a.size + b.size - 2)
+
+    if alternative == "two-sided":
+        p = 2.0 * (1.0 - stats.t.cdf(abs(t), df))
+    elif alternative == "larger":
+        p = 1.0 - stats.t.cdf(t, df)
+    elif alternative == "smaller":
+        p = stats.t.cdf(t, df)
+    else:
+        raise ValueError("unknown alternative: %s" % alternative)
+
+    return TestResult(
+        statistic=t,
+        p_value=p,
+        lift=mean_b - mean_a,
+        ci_low=None,
+        ci_high=None,
+        method="welch t",
     )
