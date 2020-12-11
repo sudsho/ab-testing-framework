a25650d1c15b2475914091f4936cf5bdcc067134
Sudhanshu Srivastava <48932266+sudsho@users.noreply.github.com>
2020-12-11 12:54:24 +0530

    credible interval + expected loss

--- src/bayes.py
+++ src/bayes.py
@@ -35,3 +35,26 @@ def prob_b_beats_a(succ_a, n_a, succ_b, n_b,
     seed_b = None if seed is None else seed + 1
     b_samples = sample_posterior(succ_b, n_b, prior, n_samples, seed_b)
     return float((b_samples > a_samples).mean())
+
+
+def credible_interval(successes, trials, prior=(1.0, 1.0), level=0.95):
+    """Equal-tailed credible interval on the rate."""
+    a, b = beta_posterior(successes, trials, *prior)
+    lo = (1.0 - level) / 2.0
+    hi = 1.0 - lo
+    return float(stats.beta.ppf(lo, a, b)), float(stats.beta.ppf(hi, a, b))
+
+
+def expected_loss(succ_a, n_a, succ_b, n_b,
+                  prior=(1.0, 1.0), n_samples=50000, seed=None):
+    """Expected loss of choosing each variant if the other one is in fact better.
+
+    Useful for "stop the test" rules; if expected loss of choosing B is below
+    a threshold (say 0.0001) you can call B the winner with low regret.
+    """
+    a_samples = sample_posterior(succ_a, n_a, prior, n_samples, seed)
+    seed_b = None if seed is None else seed + 1
+    b_samples = sample_posterior(succ_b, n_b, prior, n_samples, seed_b)
+    loss_choose_a = np.maximum(b_samples - a_samples, 0.0).mean()
+    loss_choose_b = np.maximum(a_samples - b_samples, 0.0).mean()
+    return float(loss_choose_a), float(loss_choose_b)
