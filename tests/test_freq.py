6fb9b1d6660daccffc5a528e321cb6fcad3af90c
Sudhanshu Srivastava <48932266+sudsho@users.noreply.github.com>
2020-12-22 10:07:47 +0530

    freq tests

---
+++ tests/test_freq.py
@@ -0,0 +1,33 @@
+import math
+
+import numpy as np
+import pytest
+
+from src.freq import two_proportion_ztest, welch_ttest, chi_square_test
+
+
+def test_ztest_no_difference():
+    res = two_proportion_ztest(50, 1000, 50, 1000)
+    assert abs(res.statistic) < 1e-9
+    assert res.p_value > 0.99
+
+
+def test_ztest_clear_lift():
+    res = two_proportion_ztest(50, 1000, 100, 1000)
+    assert res.p_value < 0.001
+    assert res.lift > 0
+
+
+def test_ttest_basic():
+    rng = np.random.RandomState(0)
+    a = rng.normal(0, 1, size=500)
+    b = rng.normal(0.3, 1, size=500)
+    res = welch_ttest(a, b)
+    assert res.p_value < 0.001
+    assert res.lift > 0
+
+
+def test_chi_square_2x2():
+    res = chi_square_test([[50, 950], [100, 900]])
+    assert res.p_value < 0.001
+    assert res.lift is not None
